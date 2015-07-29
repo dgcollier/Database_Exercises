2d0bce11a0a8a4411e3a14a8e5a13c859cf35ae5
dgcollier <david@gcollier.me>
2015-07-29 09:55:24 -0500

    Changed data type of release_date to CHAR to accommodate UPDATE lesson.

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,7 +6,7 @@ CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
     name VARCHAR(50) NOT NULL,
-    release_date YEAR(4) NOT NULL,
+    release_date CHAR(4) NOT NULL,
     sales_millions DECIMAL(4,1) NOT NULL,
     genre VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
