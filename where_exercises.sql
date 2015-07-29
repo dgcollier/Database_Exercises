fbcbae994db81177444d4d4d708e1f9496f3a804
dgcollier <david@gcollier.me>
2015-07-29 13:48:49 -0500

    Updated query to use OR instead of IN.

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,7 +1,9 @@
 -- Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN)
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name = 'Irena'
+    OR first_name = 'Vidya' 
+    OR first_name = 'Maya';
 
 -- Employees whose last name starts with 'E' — 7,330 rows.
 SELECT emp_no, first_name, last_name
