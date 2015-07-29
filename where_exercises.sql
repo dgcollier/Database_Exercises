2e367514e4c3bc601affbd7509af7a425bb1629c
dgcollier <david@gcollier.me>
2015-07-29 13:59:55 -0500

    Found all employees hired in the 90s who were born on Christmas

--- where_exercises.sql
+++ where_exercises.sql
@@ -23,15 +23,11 @@ FROM employees
 WHERE last_name LIKE '%e'
     AND last_name LIKE 'e%';
 
--- Employees hired in the 90s — 135,214 rows.
+-- Find all employees hired in the 90s and born on Christmas — 362 rows.
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE hire_date LIKE '199%';
-
--- Employees born on Christmas — 842 rows.
-SELECT emp_no, first_name, last_name
-FROM employees
-WHERE birth_date LIKE '%-12-25';
+WHERE hire_date LIKE '199%'
+    AND birth_date LIKE '%-12-25';
 
 -- Employees with a 'q' in their last name — 1,873 rows.
 SELECT emp_no, first_name, last_name
