477e16cd98b9314b4649c9e0591dbb4785e59b6a
dgcollier <david@gcollier.me>
2015-07-30 09:20:54 -0500

    Used GROUP BY to find unique employee last names starting with 'e'

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -1,6 +1,12 @@
 -- 1. use DISTINCT to find unique titles
 -- 2. sort alphabetically
-
 SELECT DISTINCT title
 FROM titles
 ORDER BY title;
+
+-- 1. use GROUP BY and find unique employee last names starting with "e"
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%e'
+    AND last_name LIKE 'e%'
+GROUP BY last_name;
