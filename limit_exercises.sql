d716186448b8e437b553d60cabc30542f2b8a7fa
dgcollier <david@gcollier.me>
2015-07-30 09:59:01 -0500

    Updated the query to find the tenth batch of results.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -6,9 +6,10 @@ LIMIT 10;
 
 -- Find your query for employees born on Christmas & hired in the 90s.
 -- Update it to find just the first 5 employees.
+-- Update the query to find the tenth batch of results.
 SELECT first_name, last_name
 FROM employees
 WHERE hire_date LIKE '199%'
 AND birth_date LIKE '%-12-25'
 ORDER BY birth_date ASC, hire_date DESC
-LIMIT 5;
+LIMIT 5 OFFSET 45;
